fea6b6e44c166d2ee62b13b2038db9d1719939e0
Romil Chavda <89027751+Romilchavda@users.noreply.github.com>
2024-10-11 19:55:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Clash of Clans API Token (Replace with your own)
-const API_TOKEN = 'YOUR_CLASH_OF_CLANS_API_KEY';
+const API_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImFlNzVjMjNhLTM4YWEtNDY3Ni04MDg4LWQxNDdmODI4ZmIxNyIsImlhdCI6MTcyODY1MzYyNSwic3ViIjoiZGV2ZWxvcGVyL2M2M2M0YTU2LTJjYTEtYWZhZS1kZTI1LTdlNmQxYTY0NjM3ZiIsInNjb3BlcyI6WyJjbGFzaCJdLCJsaW1pdHMiOlt7InRpZXIiOiJkZXZlbG9wZXIvc2lsdmVyIiwidHlwZSI6InRocm90dGxpbmcifSx7ImNpZHJzIjpbIjQ1Ljc5LjIxOC43OSJdLCJ0eXBlIjoiY2xpZW50In1dfQ.dHsvn0N8F0zv5_TCN0KYwwtM0sUH80cZoRECB9qyxKOw7-OJocapz9cGSTeF5PH_aALeOLN1_Pccn6rpfWel8Q';
 
 // Clan Tag (Replace with the tag of the clan you want to fetch)
-const CLAN_TAG = '#CLANTAG';
+const CLAN_TAG = '#2QGVCCPJC';
 
 // Function to fetch clan info from the Clash of Clans API
 async function fetchClanInfo() {
